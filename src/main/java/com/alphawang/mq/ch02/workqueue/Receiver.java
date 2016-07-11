95d89b24701dd14d54879459b7a6abb4c427d3fb
Alpha <awang@coupang.com>
2016-07-11 14:46:26 +0800

    [mq] ch2: work queue

--- src/main/java/com/alphawang/mq/ch02/workqueue/Receiver.java
+++ src/main/java/com/alphawang/mq/ch02/workqueue/Receiver.java
@@ -1,4 +1,4 @@
-package com.alphawang.mq.ch01.helloworld;
+package com.alphawang.mq.ch02.workqueue;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
@@ -13,39 +13,54 @@ import java.io.IOException;
 /**
  * Created by Alpha on 7/11/16.
  *
- * refer to http://www.rabbitmq.com/tutorials/tutorial-one-java.html
+ * refer to http://www.rabbitmq.com/tutorials/tutorial-two-java.html
  */
 public class Receiver {
 
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] argv)
-		throws java.io.IOException,
-		java.lang.InterruptedException {
+		throws IOException,
+		InterruptedException {
 
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		// Note that we declare the queue here, as well.
-		// Because we might start the receiver before the sender,
-		// we want to make sure the queue exists before we try to consume messages from it.
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-
-		// Since it will push us messages asynchronously,
-		// we provide a callback in the form of an object that will buffer the messages until we're ready to use them.
-		// That is what a DefaultConsumer subclass does.
-		Consumer consumer = new DefaultConsumer(channel) {
+		/**
+		 * A worker process running in the background will pop the tasks and eventually execute the job.
+		 * When you run many workers the tasks will be shared between them.
+		 *
+		 * By default, RabbitMQ will send each message to the next consumer, in sequence.
+		 * On average every consumer will get the same number of messages.
+		 * This way of distributing messages is called round-robin.
+		 */
+		final Consumer consumer = new DefaultConsumer(channel) {
 			@Override
 			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
+
 				System.out.println(" [x] Received '" + message + "'");
+				try {
+					doWork(message);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					System.out.println(" [x] Done");
+				}
 			}
 		};
 		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+
+	private static void doWork(String task) throws InterruptedException {
+		for (char ch: task.toCharArray()) {
+			if (ch == '.') Thread.sleep(1000);
+		}
 	}
 
 
