53c27bf5f41439b8055c6ed3c96468e5cca6e95d
Alpha <awang@coupang.com>
2016-07-11 17:22:16 +0800

    [mq] ch5: exchange type topic: init

---
+++ src/main/java/com/alphawang/mq/ch05/exchangetype/topic/direct/TopicPublisher.java
@@ -0,0 +1,34 @@
+package com.alphawang.mq.ch05.exchangetype.topic.direct;
+
+import com.alphawang.mq.ChannelHelper;
+import com.alphawang.mq.ch03.exchanges.LogConst;
+import com.alphawang.mq.ch04.exchangetype.direct.LogLevel;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by Alpha on 7/11/16.
+ */
+public class TopicPublisher {
+
+	public static void main(String[] args) throws IOException {
+		ChannelHelper helper = new ChannelHelper();
+		Pair<Connection, Channel> pair = helper.create();
+
+		// direct: a message goes to the queues whose binding key exactly matches the routing key of the message.
+		Channel channel = pair.getValue();
+		channel.exchangeDeclare(LogConst.EXCHANGE_NAME, "direct");
+
+		LogLevel logLevel = LogLevel.INFO;  // 每次publish, level会不一样
+		String log = "[info] test log " + Arrays.toString(args);
+		channel.basicPublish(LogConst.EXCHANGE_NAME, logLevel.name(), MessageProperties.PERSISTENT_TEXT_PLAIN, log.getBytes());
+		System.out.println("[x] Sent '" + log + "'");
+
+		helper.close(pair);
+	}
+}
---
+++ src/main/java/com/alphawang/mq/ch05/exchangetype/topic/direct/TopicSubscriber.java
@@ -0,0 +1,48 @@
+package com.alphawang.mq.ch05.exchangetype.topic.direct;
+
+import com.alphawang.mq.ChannelHelper;
+import com.alphawang.mq.ch03.exchanges.LogConst;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import javafx.util.Pair;
+
+import java.io.IOException;
+
+/**
+ * Created by Alpha on 7/11/16.
+ */
+public class TopicSubscriber {
+
+	public static void main(String[] args) throws IOException {
+		ChannelHelper helper = new ChannelHelper();
+		Pair<Connection, Channel> pair = helper.create();
+
+		Channel channel = pair.getValue();
+
+		channel.exchangeDeclare(LogConst.EXCHANGE_NAME, "direct");
+		String queueName = channel.queueDeclare().getQueue();
+
+		// 只监听指定的routing key
+		for (String level : args) {
+			String routingKey = level;
+			channel.queueBind(queueName, LogConst.EXCHANGE_NAME, routingKey);
+		}
+
+		System.out.println(" [*] Subscriber Queue '" + queueName + "'");
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope,
+				AMQP.BasicProperties properties, byte[] body) throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+			}
+		};
+		channel.basicConsume(queueName, true, consumer);
+	}
+}
