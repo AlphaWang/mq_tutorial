6b3a5e3499c57471ba87b02032afaefe5e4fe502
Alpha <awang@coupang.com>
2016-07-11 14:50:50 +0800

    [mq] ch2: work queue

--- src/main/java/com/alphawang/mq/ch01/helloworld/Receiver.java
+++ src/main/java/com/alphawang/mq/ch01/helloworld/Receiver.java
@@ -40,8 +40,7 @@ public class Receiver {
 		// That is what a DefaultConsumer subclass does.
 		Consumer consumer = new DefaultConsumer(channel) {
 			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-				throws IOException {
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				System.out.println(" [x] Received '" + message + "'");
 			}
--- src/main/java/com/alphawang/mq/ch02/workqueue/Receiver.java
+++ src/main/java/com/alphawang/mq/ch02/workqueue/Receiver.java
@@ -31,16 +31,36 @@ public class Receiver {
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-		Consumer consumer = new DefaultConsumer(channel) {
+		/**
+		 * A worker process running in the background will pop the tasks and eventually execute the job.
+		 * When you run many workers the tasks will be shared between them.
+		 *
+		 * By default, RabbitMQ will send each message to the next consumer, in sequence.
+		 * On average every consumer will get the same number of messages.
+		 * This way of distributing messages is called round-robin.
+		 */
+		final Consumer consumer = new DefaultConsumer(channel) {
 			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-				throws IOException {
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
+
 				System.out.println(" [x] Received '" + message + "'");
+				try {
+					doWork(message);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					System.out.println(" [x] Done");
+				}
 			}
 		};
 		channel.basicConsume(QUEUE_NAME, true, consumer);
 	}
 
+	private static void doWork(String task) throws InterruptedException {
+		for (char ch: task.toCharArray()) {
+			if (ch == '.') Thread.sleep(1000);
+		}
+	}
 
 }
